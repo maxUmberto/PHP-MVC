0a6399e253ff6bfb5d09571dbb4be4aa18ec4a81
Max <max.umberto97@gmail.com>
2019-05-11 14:01:03 -0300

    Adicionando comentários explicando melhor o código

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -1,24 +1,32 @@
 <?php
 
     class Core{
-        //Função que pega a URL
+        //Essa função recebe como parâmetro a URL
         public function start($urlGet){
             $acao = 'index';
-            
+
+            //Deve existir na url uma variável chamada página
             if(isset($urlGet['pagina'])){
+                //Salva na variável o que ele recebeu como parâmetro na URL. Se ele recebeu pagina=home então ele vai
+                //salvar HomeController na variável
                 $controller = ucfirst($urlGet['pagina'].'Controller');
             }else{
+                //Se não existir nenhuma variável na URL, por padrão então o controller chamado vai ser o HomeController
                 $controller = 'HomeController';
             }
 
-
+            //Conferimos se a classe que geramos logo acima existe. Se recebemos pagina=asjkd teriamos AsjkdController
+            //salvo na variável, e essa classe não existe, logo ela cai no if e chamamos o ErroController e assim
+            //mostramos uma mensagem de erro. Famoso erro 404
             if(!class_exists($controller)){
                 $controller = 'ErroController';
             }
 
-            //Passa um array onde primeiro se informa o nome da classe
-            //No segundo parâmetro se informa o nome do metodo
-            //O outro array recebe os parâmetros (id, por exemplo);
+            //essa função funciona assim. Primeiro se passa um array, nesse array nós colocamos a classe que queremos
+            //chamar e a funçaõ dentro dessa classe que desejamos chamar. O segundo array seria caso nos quisessemos
+            //passar variáveis para essa função.
+            //Por exemplo, aqui estamos intanciando a classe HomeController e chamando a função index dela, que faz
+            //alguma coisa. Nesse caso por não estarmos salvando em uma variável ela não retorna nada
             call_user_func(array(new $controller, $acao), array());
         }
     }
--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 
+//Importa os arquivos necessários para o código
 require_once 'app/Core/Core.php';
 require_once 'app/Controller/HomeController.php';
 require_once 'app/Controller/ErroController.php';
 
+//Pega o que está no arquivo estrutura.html e salva na variável template
 $template = file_get_contents('app/Template/estrutura.html');
 
+//ob_start pega todos os dados de saída e guarda em buffer
 ob_start();
     $core = new Core;
+    //Chama a função start no objeto Core passando o que está na url como parâmetro
     $core->start($_GET);
 
-    //Pega conteudo e joga na variável saída
+    //Pega o retorno do buffer e armazena na variável saída
     $saida = ob_get_contents();
+//esvazia o buffer e o encerra, nenhuma saída é enviada
 ob_end_clean();
 
+//Seguinte, nesse caso vamos procurar a ocorrencia de {{area_dinamica}} na nossa página estrura.html que está salvo na
+//variável $template e substitui pelo o que recebemos e está salvo na variável $saida
 $templatePronto = str_replace('{{area_dinamica}}', $saida, $template);
 
 echo $templatePronto;
